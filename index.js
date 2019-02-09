66f6b768abba7a2f6f351f3db06b7f76899090e4
Patricio G Maseda <pmaseda@sparkdigital.com>
2019-02-09 10:36:13 -0300

    Add fund request

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const { parse } = require('url');
 const fetch = require('node-fetch');
 
+const bloomberg = 'https://www.bloomberg.com/markets2/api/history/{{fund}}%3AAR/PX_LAST?timeframe=1_MONTH&period=daily&volumePeriod=daily';
+const validFunds = [
+  'FBARFPB'
+];
+
 module.exports = (req, res) => {
-  const { query } = parse(req.url, true);
-  const { name = 'World' } = query;
-  res.end(`Hello ${name}!`);
+  const queryParams = parse(req.url, true).query;
+
+  if (!validFunds.find(elem => elem == queryParams.fund)) {
+    return res.end(JSON.stringify({ error: 'Fund not allowed' }));
+  }
+
+  fetch(bloomberg.replace('{{fund}}', queryParams.fund), {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    }
+  })
+    .then(response => response.text())
+    .then(response => {
+      console.log(response);
+      res.end(response);
+    })
+    .catch(err => res.end(JSON.stringify({ error: err.message })));
 }
