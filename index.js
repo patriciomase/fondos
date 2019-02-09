9deaf3cff0c56872a8cc0f23f6fd977bd8b0ffee
Patricio G Maseda <pmaseda@sparkdigital.com>
2019-02-09 09:16:07 -0300

    Add nodemon for local development

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { parse } = require('url');
-const fetch = require('fetch');
+const fetch = require('node-fetch');
 
 module.exports = (req, res) => {
   const { query } = parse(req.url, true);
