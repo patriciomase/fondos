6e7699b4976852900ad66f59a8b131ab49ad0c85
Patricio G Maseda <pmaseda@sparkdigital.com>
2019-02-27 18:47:26 -0300

    Add simple memoization

--- index.js
+++ index.js
@@ -5,8 +5,39 @@ const bloomberg = 'https://www.bloomberg.com/markets2/api/history/{{fund}}%3AAR/
 const validFunds = [
   'FBAHORA',  // Horizonte
   'BFRENTP',  // Renta pesos
-  'FBARFPB'   // Renta fija plus
+  'FBARFPB',  // Renta fija plus
+  'FACCARB',  // Acciones Argentina
+  'BFBARGB',  // Bonos Argentina
+  'FBAHOPB',  // Horizonte plus
+  'FBABONB',  // Bonos globales
+  'FBARFDA',  // Renta fija dolar
+  'FRFDPLB',  // Renta fija dolar plus
+  'FBABLAA',  // Bonos latam
+  'FBARMXB',  // Renta mixta
+  'FBARTIB',  // Retorno total
+  'FBRTIIB',  // Retorno total II (USD)
+  'BFCALIF',  // Calificado
+  'BFALATB',  // Acciones Latinoamericanas
+  'FBABRAD',  // Brasil I
 ];
+
+let cache = {};
+function memoFetch(uri) {
+  if (cache[uri]) {
+    console.log('FROM CACHE', cache[uri]);
+    return Promise.resolve(cache[uri]);
+  }
+  return fetch(uri, {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    }
+  })
+  .then(response => response.text())
+  .then(r => {
+    cache[uri] = r;
+    return r;
+  });
+}
 
 module.exports = (req, res) => {
   const headers = {
@@ -29,12 +60,7 @@ module.exports = (req, res) => {
       return res.end(JSON.stringify({ error: 'Fund not allowed' }));
     }
   
-    fetch(bloomberg.replace('{{fund}}', queryParams.fund), {
-      headers: {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-      }
-    })
-      .then(response => response.text())
+    memoFetch(bloomberg.replace('{{fund}}', queryParams.fund))
       .then(response => {
         res.writeHead(200, headers);
         res.end(response);
